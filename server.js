3ec2ca49250db854ef7620b2fea6507896e6d714
fostible <aoybtt@gmail.com>
2026-04-26 23:46:56 +0700

    game 0190

--- server.js
+++ server.js
@@ -1397,7 +1397,6 @@ app.post('/api/:mode/auth/guest-init', async (req, res) => {
                 energyValue: 100, 
                 isLocked: true,
 				stackable: true,
-				quantity: 1,
                 imgKey: 'EnergyChip0' 
             });
         }
